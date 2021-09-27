19e232693364101ea1ab953e20297af3b01ef26d
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-09-27 20:00:12 +1000

    Changing speed raplay game prompt

--- index.rb
+++ index.rb
@@ -3,7 +3,7 @@
 require 'colorize'
 
 def check_answer?(right_answer, input)
-  if right_answer == input
+  if right_answer == input.to_i
     puts 'correct'.cyan.bold
     true
   else
@@ -15,36 +15,36 @@ end
 count = 0
 right_ans = 0
 wrong_ans = 0
-array = [1, 2, 3]
+numbers = [1, 2, 3]
 exit_app = false
 while exit_app == false
   system 'clear'
   puts 'Type game type to play'
   puts 'options: speed, free'
   input = gets.chomp
-
+  system 'clear'
   # start of Speed
   if input == 'free'
     puts 'Please enter player name'
     player = gets.chomp.upcase
 
-    array.each do |num|
-      ans_sample = array.sample
-      puts "#{num} + #{ans_sample} = "
-      random_number = num + ans_sample
-      user_input = gets.chomp.to_i
+    numbers.each do |num|
+      num_sample = numbers.sample
+      puts "#{num} + #{num_sample} = "
+      correct_answer = num + num_sample
+      user_input = gets.chomp
       count += 1
 
       break if user_input == 'exit'
 
-      variable = check_answer?(random_number, user_input)
+      variable = check_answer?(correct_answer, user_input)
       if variable == true
         right_ans += 1
       else
         wrong_ans += 1
       end
 
-      unless array.length > count
+      unless numbers.length > count
 
         if right_ans > 0
           puts "#{player}'s score: #{right_ans} correct #{wrong_ans} wrong".cyan.bold
@@ -64,78 +64,70 @@ while exit_app == false
         next
       elsif play_again == 'no'
         exit_app = true
-        return
+        break
       else
         puts "Incorrect input please enter 'yes' or 'no'"
         play_again = gets.chomp
       end
     end
+  
+  
   elsif input == 'speed'
     puts 'Please enter player name'
     player = gets.chomp.upcase
-    right_answers = 0
+    system 'clear'
+    right_ans = 0
     num_seconds = 3
     start_time = Time.now.to_i
     current_time = Time.now.to_i
+    wrong_ans = 0
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+    
     while current_time < start_time + num_seconds
       # p "Speed Maths 30 seconds"
       # input = gets.chomp
-      numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
-      wrong_ans = 0
       numbers.each do |num|
         num_sample = numbers.sample
+        
         if count < 6 || count > 10
-          printf "#{num} + #{num_sample} = "
-          num += num_sample
+          print "#{num} + #{num_sample} = "
+          correct_answer = num + num_sample
         else
-          printf "#{num} - #{num_sample} = "
-          num -= num_sample
+          print "#{num} - #{num_sample} = "
+          correct_answer = num - num_sample
         end
-
+        
         # getting answer
-        answer = gets.chomp
-        ans = answer.to_i
+        user_input = gets.chomp
+        system 'clear'
+
+        # exit if user quits
+        break if user_input == 'exit'
+
+        
+        
+        variable = check_answer?(correct_answer, user_input)
+        if variable == true
+          right_ans += 1
+        else
+          wrong_ans += 1
+        end
 
         # break if too slow
         if Time.now.to_i > start_time + num_seconds
+          system 'clear'
           puts "Time's up".red.bold
+          puts "#{player}'s Total score: #{right_ans} correct and #{wrong_ans} wrong".bold.colorize(right_ans.positive? ? :blue : :red)
           gets.chomp
           break
         end
-
-        # exit if user quits
-        if answer == 'exit'
-          puts 'You stay classy San Diego!'.cyan.bold
-          break
-
-        elsif num == ans
-          puts 'correct'.cyan.bold
-          right_answers += 1
-
-        else
-          puts 'wrong'.red.bold
-          wrong_ans += 1
-
-        end
-
       end
       current_time = Time.now.to_i
     end
 
-    puts "Total score #{right_answers}".blue.bold
-
     
-    # if right_answers.positive?
-    #   color = :blue
-    # else
-    #   color = :red
-    # end
-
-    # color =  right_answers.positive? ? :blue : :red
-    # puts "#{player}'s score: #{right_answers} correct and #{wrong_ans} wrong".colorize(color)
-
-    puts "#{player}'s score: #{right_answers} correct and #{wrong_ans} wrong".colorize(right_answers.positive? ? :blue : :red)
+    puts "#{player}'s score: #{right_ans} correct and #{wrong_ans} wrong".bold.colorize(right_ans.positive? ? :blue : :red)
     puts 'press enter to continue'
     gets.chomp
   end
