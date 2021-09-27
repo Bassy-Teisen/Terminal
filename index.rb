6b225cdad381fdad21707c80f2a07759c79a9b8b
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-09-27 13:26:35 +1000

    Creating a retry option

--- index.rb
+++ index.rb
@@ -1,4 +1,27 @@
 require 'colorize'
+
+def check_answer?(right_answer, input)
+    if right_answer == input
+        puts "correct".cyan.bold
+        return true
+    else 
+        puts "wrong".red.bold 
+        return false
+    end
+end
+
+def game_calcultation 
+
+    
+end
+
+
+
+
+count = 0        
+right_ans = 0
+wrong_ans = 0
+array = [1,2,3]
 exit_app = false
 while exit_app == false do 
     system "clear"
@@ -6,33 +29,28 @@ while exit_app == false do
     puts "options: speed, free" 
     input = gets.chomp
     if input == "speed"
-        player = name.upcase
-        count = 0
-        array = [1,2,3]
-        right_ans = 0
-        wrong_ans = 0
+        puts "Please enter player name"
+        player = gets.chomp.upcase
+        
+ 
         array.each do |num|
             ans_sample = array.sample 
             puts "#{num} + #{ans_sample} = "
-            num = num + ans_sample
-            answer = gets.chomp
-            ans_int = answer.to_i
+            random_number = num + ans_sample
+            user_input = gets.chomp.to_i
             count += 1
-            
-        # def answer_output
-            unless answer == "exit"
-                if num == ans_int
-                puts "correct".cyan.bold
-                right_ans += 1 
-                elsif
-                puts "wrong".red.bold 
-                wrong_ans += 1
-                else answer == "exit"   
-                end
-            else
-                puts "You stay classy San Diego!".cyan.bold
+
+            if user_input == "exit"
                 break
             end
+            
+            variable = check_answer?(random_number, user_input)
+            if variable == true
+                right_ans += 1
+            else
+                wrong_ans += 1
+            end
+            
         # end  
             unless array.length > count
         # def game_output        
@@ -42,10 +60,23 @@ while exit_app == false do
                     puts "#{player}'s score: #{right_ans} correct #{wrong_ans} wrong".red.bold
                 else 
                 puts "no answers".red.bold
-                end 
-        # end    
+                end                 
             end
-        end
+
+        
+    end
+    puts "Do you want to play again?"
+    play_again = gets.chomp
+    if play_again == "yes"
+        next
+    elsif play_again == "no"
+
+    else
+        break
+    end    
+
+    puts "not this"
+        gets.chomp
     elsif input == "free"
         puts "freeplay chosen"
     elsif input == "exit"
