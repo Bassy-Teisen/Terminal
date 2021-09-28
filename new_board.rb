47efe5d5fa6be0a97db58b7adfbf929d6391efdb
Bassy_Teisen <cab022111@coderacademy.edu.au>
2021-09-28 14:08:42 +1000

    Creating leaderbord checker

--- new_board.rb
+++ new_board.rb
@@ -1,5 +1,24 @@
 require 'csv'
+require 'smarter_csv'
 
-results = CSV.parse(File.read("filename.csv"), headers: true)
-p results["score"]
 
+
+
+data = SmarterCSV.process( "filename.csv" ) 
+ 
+p data
+foo = {:bassy => 10}
+p foo.values[0]
+def converter(input,new_result)
+    input.each do |hash|
+        hash.each do |key, val|
+            val = val.to_i
+            if val < new_result.values[0]
+            return new_result
+            else
+                next
+            end     
+        end
+    end
+end
+p converter(data,foo)
